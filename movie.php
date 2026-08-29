7335e263499a151aa8d0676ee4d53c6d74507d48
Ishika-is-here <ishikarawat802@gmail.com>
2026-08-29 23:21:40 +0530

    Update links and dropdown menu items in movie.php

--- movie.php
+++ movie.php
@@ -35,7 +35,7 @@ $isLoggedIn = isset($_SESSION['user_id']) ? 'true' : 'false';
                     </li>
 
                     <li class="dropdown">
-                        <a href="javascript:void(0)">Movies<span class="arrow">&#9656;</span></a>
+                        <a href="javascript:void(0)" class="active">Movies<span class="arrow">&#9656;</span></a>
                         <ul class="dropdown-menu">
                             <li><a href="movie.php#horror">Horror</a></li>
                             <li><a href="movie.php#sci-fi">Sci-Fi</a></li>
@@ -44,7 +44,7 @@ $isLoggedIn = isset($_SESSION['user_id']) ? 'true' : 'false';
                     </li>
 
                     <li class="dropdown">
-                        <a href="javascript:void(0)" class="active">Books<span class="arrow">&#9656;</span></a>
+                        <a href="javascript:void(0)">Books<span class="arrow">&#9656;</span></a>
                         <ul class="dropdown-menu">
                             <li><a href="book.php#fiction">Fiction</a></li>
                             <li><a href="book.php#classics">Classics</a></li>
@@ -92,7 +92,7 @@ $isLoggedIn = isset($_SESSION['user_id']) ? 'true' : 'false';
                         <th>Original Price</th>
                         <th>Discount (10%)</th>
                         <th>Total Paid</th>
-                        <th>Action</th> </tr>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody id="libraryTableBody">
@@ -120,16 +120,16 @@ $isLoggedIn = isset($_SESSION['user_id']) ? 'true' : 'false';
     <footer>
     <div class="footer-container">
         <div class="footer-left">
-        <a href="about.html#feedback-section">Contact Us</a>
-        <a href="about.html#feedback-section">Feedback</a>
+        <a href="about.php#feedback-section">Contact Us</a>
+        <a href="about.php#feedback-section">Feedback</a>
         <a href="/terms">Terms & Policy</a>
         </div>
 
         <div class="footer-right">
-        <a href="#" class="social-icon"><i class="fa-brands fa-instagram"></i></a>
-        <a href="#" class="social-icon"><i class="fa-brands fa-x-twitter"></i></a>
-        <a href="#" class="social-icon"><i class="fa-brands fa-youtube"></i></a>
-        <a href="#" class="social-icon"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="https://www.instagram.com/?hl=en" class="social-icon"><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://x.com" class="social-icon"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="https://www.youtube.com" class="social-icon"><i class="fa-brands fa-youtube"></i></a>
+        <a href="https://www.facebook.com" class="social-icon"><i class="fa-brands fa-square-facebook"></i></a>
         </div>
   </div>
 </footer>
